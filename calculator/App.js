8e3bb1ae4c6cea9430c41ab51116687df2b4cc99
praneethguptha <123263232+hemanthkotha1198@users.noreply.github.com>
2023-07-31 10:49:56 +0530

    Add files via upload

---
+++ calculator/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Index from './Component/Index';
+import Index from './Component/Index';
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      
+    </div>
+  );
+}
+
+export default App;
